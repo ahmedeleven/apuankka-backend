145fa22e17b1cd794fb0747c8a5fb6d261f03a3e
Ahmed <ahmed.massod@gmail.com>
2023-09-21 14:54:09 +0300

    add database settings

--- apuankka/settings.py
+++ apuankka/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'apuankka.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'NAME': 'apuankka',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
